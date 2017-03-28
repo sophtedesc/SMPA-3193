31bfe3a6edb4fb4065f90dedae97a7f5083c714a
sophtedesc <sophia19@gwu.edu>
2017-03-28 00:13:10 -0400

    Columbian College scrape.py
    Until I figure out how to generate content for the Academic Year column, I decided to leave it out so that its contents wouldn't have the Department list

--- scrape.py
+++ scrape.py
@@ -2,7 +2,8 @@ import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
-url = 'http://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html'
+url = 'https://columbian.gwu.edu/2015-2016'
+url = 'https://columbian.gwu.edu/2010-2011'
 response = requests.get(url)
 html = response.content
 
@@ -10,13 +11,13 @@ soup = BeautifulSoup(html)
 table = soup.find('table')
 
 list_of_rows = []
-for row in table.findAll('tr')[1:-1]:
+for row in table.findAll('tr')[1:]:
     list_of_cells = []
     for cell in row.findAll('td'):
-    	list_of_cells.append(cell.text)
+    	list_of_cells.append(cell.text.encode('utf-8'))
     list_of_rows.append(list_of_cells)
     
-outfile = open("executions.csv", "wb")
+outfile = open("grants.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerow(["scheduled_execution", "link", "last_name", "first_name", "TDCJ_number", "date_of_birth", "race", "date_received", "county"])
+writer.writerow(["Department", "Faculty", "Sponsor", "Title"])
 writer.writerows(list_of_rows)
