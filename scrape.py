244e9244bb21fd274e25a205efeb2353ecb018d2
sophtedesc <sophia19@gwu.edu>
2017-03-28 12:09:25 -0400

    MOST RECENT scrape.py under college folder

--- scrape.py
+++ scrape.py
@@ -2,22 +2,26 @@ import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
-url = 'https://columbian.gwu.edu/2015-2016'
-url = 'https://columbian.gwu.edu/2010-2011'
-response = requests.get(url)
-html = response.content
 
-soup = BeautifulSoup(html)
-table = soup.find('table')
-
+years = ['2015-2016', '2014-2015', '2013-2014', '2012-2013', '2011-2012', '2010-2011']
 list_of_rows = []
-for row in table.findAll('tr')[1:]:
-    list_of_cells = []
-    for cell in row.findAll('td'):
-    	list_of_cells.append(cell.text.encode('utf-8'))
-    list_of_rows.append(list_of_cells)
+
+for year in years:
+	print year
+	response = requests.get("https://columbian.gwu.edu/" + year)
+	html = response.content
+
+	soup = BeautifulSoup(html)
+	table = soup.find('table')
+
+	for row in table.findAll('tr')[1:]:
+		list_of_cells = []
+		list_of_cells.append(year)
+		for cell in row.findAll('td'):
+			list_of_cells.append(cell.text.encode('utf-8'))
+		list_of_rows.append(list_of_cells)
     
 outfile = open("grants.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerow(["Department", "Faculty", "Sponsor", "Title"])
+writer.writerow(["Year", "Department", "Faculty", "Sponsor", "Title"])
 writer.writerows(list_of_rows)
